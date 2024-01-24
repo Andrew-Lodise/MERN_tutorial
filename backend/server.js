aa52ba46fcf70f7e3fb79d0bd54a34f8f4165d4b
Andrew-Lodise <AL964440@wcupa.edu>
2024-01-23 21:30:16 -0500

    implemented middlewear functino and dotenv

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+
+const express = require("express")
+
+// creating express app
+const app = express()
+
+// middlewear, must do this before interacting with a request
+app.use( (request, response, next) => {
+    console.log(request.path, request.method)
+    next()
+})
+
+// rout handler to react to get requests
+app.get("/", (request, response) => {
+    response.json({msg: "welcome to the app bro"})
+})
+
+// listen for requests
+app.listen(3008, () => {
+    console.log("listening on port 3008!")
+})
