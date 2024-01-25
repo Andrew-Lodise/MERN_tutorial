100e79496c10e0726262831169e8718825dde5a3
Andrew-Lodise <AL964440@wcupa.edu>
2024-01-25 09:21:14 -0500

    css and navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-
+import Navbar from "./components/Navbar"
 // pages and components
 import Home from "./pages/home"
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <div className="pages">
           <Routes>
             <Route 
