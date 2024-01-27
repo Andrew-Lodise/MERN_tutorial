480f4bdd0554f2c7b7519b2067a9b6c32e20f1e6
Andrew-Lodise <AL964440@wcupa.edu>
2024-01-26 21:08:00 -0500

    added new workout form

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,45 @@
-const Home = () => {
-    return (
-        <div className="home">
-            <h2>
-                Home page!
-            </h2>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
 
-export default Home
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
+
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+        try {
+          const response = await fetch("/api/workouts");
+      
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+      
+          const json = await response.json();
+          setWorkouts(json);
+        } catch (error) {
+          console.error("Error fetching workouts:", error);
+        }
+      };
+      
+
+    fetchWorkouts();
+  }, []); // only makes it fire once
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout}/> 
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
+
+export default Home;
